9064c2d98207c74752ab36fce3f5d2b6075e3639
AngeloGi <giann.aggelos@gmail.com>
2018-11-16 02:54:37 +0200

    small fix to paddle  positions

--- Project/GameScreen.gd
+++ Project/GameScreen.gd
@@ -9,8 +9,8 @@ var int_RightPlayerScore = 0
 func _ready():
 	object_LeftPaddle = load("res://Paddle.tscn").instance()
 	object_RightPaddle = load("res://Paddle.tscn").instance()
-	object_LeftPaddle.spawnLocation(80,240)
-	object_RightPaddle.spawnLocation(880,240)
+	object_LeftPaddle.spawnLocation(100,240)
+	object_RightPaddle.spawnLocation(860,240)
 	object_LeftPaddle.setIsLeftPaddle(true)
 	object_RightPaddle.setIsLeftPaddle(false)
 	add_child(object_LeftPaddle)
